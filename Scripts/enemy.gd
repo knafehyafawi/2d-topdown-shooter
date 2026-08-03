5df4fbb407a626bd98a48be4c51eecaefb2ef471
Adnan Awad <adnanawad315@gmail.com>
2026-08-03 15:48:29 -0400

    basic enemy now has 3 hp and dies when bullet collides 3 times. bullet also does not push enemy back. bullet is unloaded when it collides with enemy.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,30 +1,29 @@
 extends CharacterBody2D
 
+@export var max_health: int = 3
+var health: int
+
 #const SPEED = 300.0
 var motion = Vector2()
 
 #@onready var player: PackedScene = preload("res://Scenes/player.tscn")
 
 func _ready() -> void:
-	pass
+	health = max_health
 
 func _physics_process(delta: float) -> void:
 	var player = get_parent().get_node("Player")
-	
-	position += (player.position-position)/50
-	
-	#velocity = player.position.normalized() * motion
-	
+	position += (player.position - position) / 50
 	look_at(player.position)
 	move_and_slide()
 
-# TO DO:
-# if bullet hits enemy, enemy take damage
-# enemy has 5 max HP. bullet does 1 dmg
-# also fix enemy spawn poss
+func take_damage(amount: int) -> void:
+	health -= amount
+	if health <= 0:
+		queue_free()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Something entered: ", body.name)
-	if "Bullet" in body.name:
-		queue_free()
+	if body.is_in_group("bullets"):
+		body.queue_free()
+		take_damage(1)
