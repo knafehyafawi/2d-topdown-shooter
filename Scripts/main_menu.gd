b6eb45448b1ff8be288bb451330e6aa2967675f2
Adnan Awad <adnanawad315@gmail.com>
2026-08-05 21:59:59 -0400

    added panel bg for the title screen and reorganized some directories again
    todo: credits submenu, options menu, transitions to game, audio implementation

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,11 +1,25 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_itchio_button_pressed() -> void:
+	OS.shell_open("https://knafehyafawi.itch.io/")
+
+
+func _on_git_hub_button_pressed() -> void:
+	OS.shell_open("https://github.com/knafehyafawi/2d-topdown-shooter")
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
